0bd7edbf0ffe25077357735eeef32dbde8ee4237
Benjamin Turner <benjamin@passionsplay.com>
2020-11-19 12:33:13 -0800

    A quick wip for making a request to Local's GraphQL endpoint

--- lwp-emacs-client.el
+++ lwp-emacs-client.el
@@ -21,3 +21,31 @@
 		 lwp-graphql-url
 		 lwp-graphql-subscriptionUrl))
 
+
+;; See: https://tkf.github.io/emacs-request/
+;;   refactorz meee!
+;;
+;; This gets the job done, but I need to refactor to:
+;; 1. use the authToken var above
+;; 2. Open response in new, dedicated buffer
+;; 3. Use dedicated functions for making the specific requests
+;; 4. Guard against users not having the request package installed
+(request
+  lwp-graphql-url
+  :type "POST"
+  :headers '(
+	     ("Content-Type" . "application/json")
+	     ("Authorization" . "Bearer kTe4tOu8J9glqG8bDklYey7nx623qXO+yh54duXUExJ1XeJGxvFOo6MAdOWdP5CBepTekj7p6if06hsyF4Zwcr0BcseoWratYlH39aoShtrJElJetMsPVX8+S3N9WToE")
+	     )
+  :data (json-encode '(("query" . "{
+    sites {
+	id
+	host
+	status
+    }
+  }")))
+  :parser 'json-read
+  :complete (cl-function
+	     (lambda (&key response &allow-other-keys)
+	       (message "Done: %s" (request-response-data response)))))
+
